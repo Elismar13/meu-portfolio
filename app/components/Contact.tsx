885988cd9ee5bd162fd6a5bc8841a511bd1e3efd
Elismar Silva <elismarsilva.5@outlook.com>
2024-07-08 08:44:46 -0300

    feat: contact page structure

---
+++ app/components/Contact.tsx
@@ -0,0 +1,65 @@
+// components/Contact.tsx
+import React from 'react';
+
+const Contact: React.FC = () => {
+  return (
+    <section id="contact" className="w-full flex flex-col lg:flex-row items-center justify-between bg-gray-800 text-white p-8">
+      <div className="lg:w-2/3 text-center lg:text-left mb-8 lg:mb-0 max-w-4xl mx-auto">
+        <h1 className="text-3xl font-bold mb-4">Get in Touch</h1>
+        <h2 className="text-3xl font-bold mb-4">Don't be Shy</h2>
+        <p className="mb-4">
+          If you liked my work, please fill out the form on the side and send me an email. If you prefer, you can also contact me via WhatsApp below:
+        </p>
+        <div className="flex flex-col lg:flex-row items-center lg:items-start">
+          <div className="flex items-center mb-2">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-green-500 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 10l6.29 6.29a1 1 0 001.42 0L21 6" />
+            </svg>
+            <div>
+              <span>WhatsApp</span>
+              <span>(83) 98872-9338</span>
+            </div>
+          </div>
+        </div>
+
+        <form className="p-8 rounded-2xl bg-gray-800">
+          <div className="mb-4">
+            <input
+              className="w-full p-3 bg-gray-300 border rounded-2xl border-transparent placeholder-gray-600 text-black"
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Name"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              className="w-full p-3 bg-gray-300 border rounded-2xl border-transparent placeholder-gray-600 text-black"
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <textarea
+              className="w-full p-3 bg-gray-300 border rounded-2xl border-transparent placeholder-gray-600 text-black"
+              id="message"
+              name="message"
+              rows={4}
+              placeholder="Message"
+              required
+            ></textarea>
+          </div>
+          <button className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">
+            Send
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,19 @@
-import Image from 'next/image';
 import Me from './components/Me';
 import Header from './components/Header';
 import About from './components/About';
 import MyProjects from './components/MyProjects';
 import Experience from './components/Experience';
+import Contact from './components/Contact';
 
 export default function Home() {
   return (
-    <main lang="en">
-      <section max-w-screen-lg mx-auto>
-        <Header />
-        <Me />
-        <About />
-        <MyProjects />
-        <Experience />
-      </section>
+    <main lang="en" max-w-screen-lg mx-auto>
+      <Header />
+      <Me />
+      <About />
+      <MyProjects />
+      <Experience />
+      <Contact />
     </main>
   );
 }
