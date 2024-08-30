cc1f549b99ecb0ac7de892ad1f9c6d9e6c810d21
Elismar Silva <elismarsilva.5@outlook.com>
2024-08-30 18:41:34 -0300

    feat: creating project modal

--- data/myProjects.ts
+++ data/myProjects.ts
@@ -1,33 +1,51 @@
 export interface Project {
   title: string;
-  link: string;
+  description: string;
+  github: string
   image: string;
+  technologies: string[];
+  deploy?: string,
 };
 
 export const projects: Project[] = [
   {
     title: "ASMonitor",
-    link: "link_projeto",
+    description: "Descrição",
+    github: "link_projeto",
     image: "..//src/assets/asmonitor.png",
+    technologies: ["React", "Java"],
+    deploy: "url",
   },
   {
     title: "Gethub",
-    link: "link_projeto",
+    description: "Descrição",
+    github: "link_projeto",
     image: "../src/assets/gethub.png",
+    technologies: ["React", "Java"],
+    deploy: "url",
   },
   {
     title: "inHome",
-    link: "link_projeto",
+    description: "Descrição",
+    github: "link_projeto",
     image: "../src/assets/inhome.png",
+    technologies: ["React", "Java"],
+    deploy: "url",
   },
   {
     title: "Transaction balance",
-    link: "link_projeto",
+    description: "Descrição",
+    github: "link_projeto",
     image: "../src/assets/transaction.png",
+    technologies: ["React", "Java"],
+    deploy: "url",
   },
   {
     title: "OpenSnake",
-    link: "link_projeto",
+    description: "Descrição",
+    github: "link_projeto",
     image: "../src/assets/opensnake.png",
+    technologies: ["React", "Java"],
+    deploy: "url",
   }
 ];
--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,36 +1,54 @@
-import React from 'react';
+// ProjectList.tsx
+import React, { useState } from 'react';
 import { Project } from '../../data/myProjects';
+import ProjectModal from './ProjectModal';
 
 interface ProjectListProps {
   projects: Project[];
 }
 
 const ProjectList: React.FC<ProjectListProps> = ({ projects }) => {
+  const [selectedProject, setSelectedProject] = useState<Project | null>(null);
+
+  const openModal = (project: Project) => {
+    setSelectedProject(project);
+  };
+
+  const closeModal = () => {
+    setSelectedProject(null);
+  };
+
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 justify-center content-center">
-      {projects.map((project, index) => (
-        <div
-          key={index}
-          className="relative group rounded shadow-md w-full max-w-md mx-auto"
-        >
-          <div className="relative w-full">
-            <img
-              src={project.image}
-              alt={project.title}
-              className="rounded-md group-hover:opacity-75 transition duration-300"
-            />
-          </div>
-          <div className="absolute inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 opacity-0 group-hover:opacity-100 transition duration-300">
-            <div className="text-center">
-              <h3 className="text-xl font-bold mb-2 text-white">{project.title}</h3>
-              <a href={project.link} className="text-blue-500 hover:underline" target="_blank" rel="noopener noreferrer">
-                View Project
-              </a>
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 justify-center content-center">
+        {projects.map((project, index) => (
+          <div
+            key={index}
+            className="relative group rounded shadow-md w-full max-w-md mx-auto"
+          >
+            <div className="relative w-full">
+              <img
+                src={project.image}
+                alt={project.title}
+                className="rounded-md group-hover:opacity-75 transition duration-300"
+              />
+            </div>
+            <div className="absolute inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 opacity-0 group-hover:opacity-100 transition duration-300">
+              <div className="text-center">
+                <h3 className="text-xl font-bold mb-2 text-white">{project.title}</h3>
+                <button onClick={() => openModal(project)} className="text-blue-500 hover:underline">
+                  View Project
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+
+      {selectedProject && (
+        <ProjectModal project={selectedProject} onClose={closeModal} />
+      )}
+    </>
   );
 };
 
