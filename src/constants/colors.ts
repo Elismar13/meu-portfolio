c32631f661a730c092118d590fe63143783bd8e9
Elismar Silva <elismarsilva.5@outlook.com>
2024-09-02 16:39:51 -0300

    feat: text custom colors

---
+++ src/constants/colors.ts
@@ -0,0 +1,9 @@
+export const COLORS = {
+  blue: '#3490dc',
+  yellow: '#ffed4a',
+  red: '#e3342f',
+  green: '#38c172',
+  purple: '#6f42c1',
+  indigo: '#6574cd',
+  pink: '#f66d9b',
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: '#3490dc',
+        yellow: '#ffed4a',
+        red: '#e3342f',
+        green: '#38c172',
+        purple: '#6f42c1',
+        indigo: '#6574cd',
+        pink: '#f66d9b',
+      }
+    },
   },
   plugins: [],
   darkMode: 'class',
