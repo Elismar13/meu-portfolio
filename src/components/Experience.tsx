d99e6d5bae81aa4e8a73fdb3ff090d4db8a6fe7c
Elismar Silva <elismarsilva.5@outlook.com>
2024-09-13 10:36:27 -0300

    feat: experience language support

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { useTranslation } from 'react-i18next';
 import ThemeContext from '../context/ThemeContext';
 import { COLORS } from '../constants/colors';
 import { experiences } from '../../data/experience';
@@ -6,12 +7,14 @@ import { FaBriefcase, FaGraduationCap } from 'react-icons/fa';
 
 const Experience: React.FC = () => {
   const { color } = useContext(ThemeContext) || { color: COLORS.CUSTOM_BLUE };
+  const { t } = useTranslation();
 
   return (
     <section id="experience" className="p-8">
       <div className="max-w-4xl mx-auto">
         <h2 className="text-3xl font-bold mb-8 text-center">
-          Experience & <span style={{ color }}>Education</span>
+          {t('experience.title')}
+          <span style={{ color }}>{t('experience.title2')}</span>
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
           {experiences.map((exp, index) => (
