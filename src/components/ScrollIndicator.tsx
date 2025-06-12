2af656e46d416f49a8ff37c98df0159220009450
Elismar Silva <elismarsilva.5@outlook.com>
2025-06-12 15:55:22 -0300

    feat: implement smooth scrolling to About section in ScrollIndicator component

--- src/components/ScrollIndicator.tsx
+++ src/components/ScrollIndicator.tsx
@@ -2,10 +2,30 @@ import React from "react";
 import { FiChevronDown } from "react-icons/fi";
 
 const ScrollIndicator: React.FC = () => {
+const handleClick = () => {
+  const aboutSection = document.getElementById("about");
+  const header = document.querySelector("header");
+
+  if (aboutSection) {
+    const sectionTop = aboutSection.getBoundingClientRect().top + window.scrollY;
+
+    const headerHeight = header?.getBoundingClientRect().height || 0;
+
+    window.scrollTo({
+      top: sectionTop - headerHeight,
+      behavior: "smooth",
+    });
+  }
+};
+
   return (
-    <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 animate-bounce-smooth cursor-pointer">
+    <button 
+      title="scroll-down"
+      className="absolute bottom-4 left-1/2 transform -translate-x-1/2 animate-bounce-smooth cursor-pointer"
+      onClick={handleClick}
+    >
       <FiChevronDown size={28} className="text-gray-500 dark:text-gray-300" />
-    </div>
+    </button>
   );
 };
 
