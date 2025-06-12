e0903f8cb949e6cf9d0b925d00d2a25fe99c7af1
Elismar Silva <elismarsilva.5@outlook.com>
2025-06-12 15:20:34 -0300

    feat: add internationalization support for button text in AnimatedButton component

--- src/components/AnimatedButton.tsx
+++ src/components/AnimatedButton.tsx
@@ -2,9 +2,11 @@ import React, { useContext } from 'react';
 import { FiSend } from 'react-icons/fi';
 import { COLORS } from '../constants/colors';
 import ThemeContext from '../context/ThemeContext';
+import { useTranslation } from 'react-i18next';
 
 const AnimatedButton: React.FC = () => {
   const { color } = useContext(ThemeContext) || { color: COLORS.CUSTOM_BLUE };
+  const { t } = useTranslation();
   
   return (
     <button
@@ -13,7 +15,7 @@ const AnimatedButton: React.FC = () => {
     >
       <span style={{ background: color }} className="absolute inset-0 w-full h-full transition-transform duration-500 ease-in-out transform translate-x-full group-hover:translate-x-0"></span>
       <span className="relative z-10 text-gray-700 dark:text-gray-100 mr-6 transition-opacity duration-500 ease-in-out ">
-        Send
+        {t('contact.send')}
       </span>
       <div style={{ background: color }} className="relative rounded-full h-full p-4">
         <FiSend className="relative z-10 text-gray-200 text-xl transition-transform duration-500 ease-in-out transform translate-x-0 group-hover:translate-x-0 group-hover:text-white" />
